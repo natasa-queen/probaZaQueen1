68de27c74c72def2b9244f26061e648fb84d669c
A.M <andrej2000aa@gmail.com>
2023-09-04 13:47:11 +0200

    ubacen static cms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react"
+import { Script } from 'gatsby'
 import { Seo } from '../components/Seo'
 import Layout from '../components/Layout'
 
 export default function Home() {
   return(
       <Layout>
+        <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
         <h1>Zdravo svete!</h1>
       </Layout>
   )
